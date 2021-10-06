32cae6db957a18fcf3e13ca76f2cbf2caa4bbbb8
vishnu191 <vishnumunnangi@gmail.com>
2021-10-05 20:56:30 -0400

    Implementation for Get and Post

--- src/main/java/com/employee/controller/EmployeeController.java
+++ src/main/java/com/employee/controller/EmployeeController.java
@@ -1,14 +1,18 @@
 package com.employee.controller;
 
+import com.employee.model.Employee;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+
 public class EmplpyeeController {
 
     @RequestMapping(value = "/employees", method = RequestMethod.GET, produces = "application/json")
-    public String helloWorld() {
+    public ResponseEntity<Employee> getAllEmployees() {
         return "Hello World! ";
     }
 }
