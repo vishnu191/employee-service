445d743d5a3bc578cee4e9c079696f8268108ef6
10936516 <priya.munnangi@autozone.com>
2021-10-06 17:22:23 -0400

    changes to allow cors

--- src/main/java/com/employee/EmployeeServiceApplication.java
+++ src/main/java/com/employee/EmployeeServiceApplication.java
@@ -2,12 +2,25 @@ package com.employee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @SpringBootApplication
 public class EmployeeServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeServiceApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
 	}
 
 }
